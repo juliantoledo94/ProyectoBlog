8604e4bf0b0ab9e2f8dbe59fa5b23d3389f5389b
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2022-01-19 21:38:31 -0300

    Estructura inicial del proyecto
    Se creo el proyecto y todas las apps necesarias

--- ProyectoBlog/settings.py
+++ ProyectoBlog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "loginApp",
+    "mensajeriaApp",
+    "perfilesApp",
+    "registroApp",
 ]
 
 MIDDLEWARE = [
